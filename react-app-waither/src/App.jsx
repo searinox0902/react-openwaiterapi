94169b710ee847be9338e7bbd8c40d45be4dc18c
unknown <searinox0902>
2023-05-04 23:13:51 -0500

    Add UI dev components : header and info-card

--- react-app-waither/src/App.jsx
+++ react-app-waither/src/App.jsx
@@ -3,15 +3,45 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+//components 
+
+import Header from './components/header'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1>Aplicación de Clima {count}</h1>
-     <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam dolores error quidem et suscipit, magnam adipisci earum ratione quasi praesentium deleniti incidunt! Ullam, itaque quam explicabo exercitationem fuga doloremque soluta!
-     </p>
+      <div className='w-full flex justify-center px-3'>
+
+        <div className='w-full md:w-1/3'>
+          <Header/>
+
+          {/* waither result consult */}
+          <h2 className='card__city-name'>Tarazá</h2>
+          <div className='card-info flex'>
+            <div className='card__content-info'>
+            <p className='card__date'>Jueves, 4 de mayo</p>
+              <p className='card__temp'>32c</p>
+              <p className='card__temp-max'>Temp Máx: 38c</p>
+              <p className='card__temp-min'>Temp Mín: 28c</p>
+            </div>
+            <div className='card__content-image'>
+              <figure>
+                <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Weather-overcast-rare-showers.svg/1200px-Weather-overcast-rare-showers.svg.png' alt='algo'></img>
+              </figure>
+            </div>
+          </div>
+
+          {/* form */}
+
+        
+
+           {/* dev tech description and dependences*/}
+        </div>
+        
+      </div>
+    
     </>
   )
 }
---
+++ react-app-waither/src/components/header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+
+    return (
+        <>
+            <h1>Consulta Clima App</h1>
+            <p className="dev-description"> By: Sebastian Vargas</p>
+        </>
+    )
+}
+
+export default Header;
