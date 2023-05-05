2b513da14c8c51e9f2ff647c302916f4ae06033b
unknown <searinox0902>
2023-05-04 22:14:59 -0500

    Add taildwind dependences and config

--- react-app-waither/src/App.jsx
+++ react-app-waither/src/App.jsx
@@ -8,26 +8,10 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1>Aplicación de Clima {count}</h1>
+     <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam dolores error quidem et suscipit, magnam adipisci earum ratione quasi praesentium deleniti incidunt! Ullam, itaque quam explicabo exercitationem fuga doloremque soluta!
+     </p>
     </>
   )
 }
